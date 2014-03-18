77362b1a806c154f2d9c84aea9dc12e3ebd5d04c
Shinmera <shinmera@tymoon.eu>
2014-03-18 13:17:47 +0100

    Updated to fast compute.

--- ex4-shin.lisp
+++ ex4-shin.lisp
@@ -19,7 +19,54 @@
                    do (decf (fill-pointer data))) ; Pop here so we have to sort less
           finally (return sum))))
 
+;; Faster using binary search insert
+(defun find-place (value array)
+  (let ((low 0)
+        (high (1- (length array))))
+    (do () ((< high low) nil)
+      (let ((middle (floor (/ (+ low high) 2))))
+        (cond ((and (= middle 0)
+                    (> (aref array middle) value))
+               (return 0))
+              ((and (> (aref array middle) value)
+                    (< (aref array (1- middle)) value))
+               (return middle))
+              ((> (aref array middle) value)
+               (setf high (1- middle)))
+              ((< (aref array middle) value)
+               (setf low (1+ middle)))
+              (t (return middle)))))))
+
+(defun insert-sorted (item target)
+  (vector-push-position item target (or (find-place item target) (fill-pointer target))))
+
+(defun compute-fast (data)
+  (let ((data (sort data #'<)))
+    (declare ((vector fixnum) data))
+    (loop with sum = 0
+          while (< 2 (fill-pointer data))
+          do (let ((limit (elt data (- (fill-pointer data) 3)))) ;; Third is our limiting factor
+               (incf sum limit)
+               (let ((el1 (- (vector-pop data) limit))
+                     (el2 (- (vector-pop data) limit)))
+                 (decf (fill-pointer data))
+                 (insert-sorted el1 data)
+                 (insert-sorted el2 data)))
+          finally (return sum))))
+
 ;; Utils
+(defun array-shift (array &key (n 1) (from 0) (to (length array)))
+  (incf (fill-pointer array) n)
+  (loop repeat (- to from)
+        for cursor downfrom (1- to)
+        do (setf (aref array (+ cursor n))
+                 (aref array cursor)))
+  array)
+
+(defun vector-push-position (element vector position)
+  (array-shift vector :from position)
+  (setf (aref vector position) element))
+
 (defun read-test-data (&optional (path #p"~/Documents/dox/Exercises/datastructures and algorithms/ex4-hard.in"))
   (with-open-file (stream path :direction :input)
     (loop repeat (read-from-string (read-line stream))
@@ -34,4 +81,4 @@
 
 (defun test-hard ()
   (loop for dat in *test*
-        do (compute dat)))
+        do (compute-fast dat)))
